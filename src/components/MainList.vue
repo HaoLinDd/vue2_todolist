b335380b4f473f37f9fda733ba20ad172dadcbae
haolin <2508963257@qq.com>
2024-05-05 12:55:18 +0800

    静态页搭建完成

---
+++ src/components/MainList.vue
@@ -0,0 +1,37 @@
+<template>
+  <ul class="todo-main">
+    <item-list/>
+    <item-list/>
+    <item-list/>
+  </ul>
+</template>
+
+<script>
+import ItemList from "./ItemList";
+export default {
+  name: 'MainList',
+  components: { ItemList },
+  props: {
+    
+  }
+}
+</script>
+
+<style scoped>
+/*main*/
+.todo-main {
+  margin-left: 0px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding: 0px;
+}
+
+.todo-empty {
+  height: 40px;
+  line-height: 40px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding-left: 5px;
+  margin-top: 10px;
+}
+</style>
